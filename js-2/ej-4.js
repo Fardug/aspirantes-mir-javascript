ed191dab4e3b3a5f1773f8911a2281ab4b889389
Fidel Hernandez <126029627+Fardug@users.noreply.github.com>
2023-03-13 22:41:29 -0500

    Update ej-4.js

--- js-2/ej-4.js
+++ js-2/ej-4.js
@@ -1,12 +1,13 @@
 function join(array){
-    if (array.length === 0) return '""';
-     let str = "";
-
-    for (let word of array){
-    str += word + " ";
-    }
-    return str;
+    if (array.length === 0) return "";
+    
+        let str = "";
+        for (let word of array){
+        str += word + " ";
+        }
+        return str;
 }
+
 console.log(join(["Hola", "Mundo"])); 
 console.log(join(["Make", "It", "Real"])); 
 console.log(join([])); 
