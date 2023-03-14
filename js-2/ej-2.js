cb3b7fa59f4e436a9d57a542692d36cb9d7000b3
Fidel Hernandez <126029627+Fardug@users.noreply.github.com>
2023-03-13 22:45:00 -0500

    Update ej-2.js

--- js-2/ej-2.js
+++ js-2/ej-2.js
@@ -1,14 +1,15 @@
 function max(array){ 
     if (array.length === 0)return undefined;
-     let maxnum = array[0];
+    let maxnum = array[0];
 
-     for (let i=1; i< array.length; i++){
-        if (array[i] > maxnum){
+    for (let i=1; i< array.length; i++){
+         if (array[i] > maxnum){
             maxnum= array[i];
-        }
-     }
+         }
+    }
      return maxnum;
 }
+
 console.log(max([1, 3, 2]));
 console.log(max([10, 9, 8, 7, 6, 5, 4]));
 console.log(max([]));
