0fe664ffbfc29c351ac20f4652faeeb1ac5019d2
Fidel Hernandez <126029627+Fardug@users.noreply.github.com>
2023-03-13 22:45:47 -0500

    Update ej-1.js

--- js-2/ej-1.js
+++ js-2/ej-1.js
@@ -1,6 +1,7 @@
 
 function sum(array){ 
     if (array.length === 0)return 0;
+    
     let total =0;
     
     for ( let num=0; num<array.length; num++ ){
@@ -8,6 +9,7 @@ function sum(array){
     }
     return total;
   } 
+
   console.log(sum([1, 2, 3]));
   console.log(sum([10, 8, 12, 5]));
   console.log(sum([]));
