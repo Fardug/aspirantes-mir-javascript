8b7d998bb7b4cf255004f0e052dc608304e7fc4d
Fidel <fidel9520@gmail.com>
2023-03-13 20:33:33 -0500

    Cambios 4

--- js-2/ej-4.js
+++ js-2/ej-4.js
@@ -1,10 +1,11 @@
 function join(array){
     if (array.length === 0) return '""';
      let str = "";
-        for (let word of array){
-        str += word + " ";
+
+    for (let word of array){
+    str += word + " ";
     }
-         return str;
+    return str;
 }
 console.log(join(["Hola", "Mundo"])); 
 console.log(join(["Make", "It", "Real"])); 
